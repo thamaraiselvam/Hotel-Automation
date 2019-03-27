cab0b2ec5ec42ca8323c2d9f5a96206fdc34e0bb
thamaraiselvam <thamaraiselvam@live.com>
2019-03-28 01:30:34 +0530

    reduce power usage state

--- controller/floor.js
+++ controller/floor.js
@@ -1,46 +1,169 @@
+const config = require('./../config/config.json');
+const _ = require('lodash');
 class Floor {
-    constructor(mainCorridor, subCorridor) {
-        this.initData(mainCorridor, subCorridor);
+    constructor(input) {
+        this.initData(input);
         this.setDefaultState();
     }
 
-    initData(mainCorridor, subCorridor) {
-        this.mainCorridor = mainCorridor;
-        this.subCorridor = subCorridor;
-        this.state = [{
-            name: 'Floor 1',
-        }];
+    initData(input) {
+        this.input = input;
+        this.state = this.currentFloor = this.currentSubCorridor = [];
+        this.currentFloorNumber = this.currentSubCorridorNumber = 0;
     }
 
     setDefaultState() {
-        this.state[0]['mainCorridor'] = [];
-        for (let i = 0; i < this.mainCorridor; i++) {
-            this.state[0]['mainCorridor'][i] = [{
-                light: 'ON',
-                AC: 'ON'
-            }]
+        for (let index = 0; index < this.input.floor; index++) {
+            this.state[index] = {};
+            this.state[index]['mainCorridor'] = [];
+            for (let innerIndex = 0; innerIndex < this.input.mainCorridor; innerIndex++) {
+                this.state[index]['mainCorridor'][innerIndex] = {
+                    light: 'OFF',
+                    AC: 'ON'
+                }
+            }
+            this.state[index]['subCorridor'] = [];
+            for (let innerIndex = 0; innerIndex < this.input.subCorridor; innerIndex++) {
+                this.state[index]['subCorridor'][innerIndex] = {
+                    light: 'OFF',
+                    AC: 'ON'
+                }
+            }
         }
-        this.state[0]['subCorridor'] = [];
-        for (let i = 0; i < this.subCorridor; i++) {
-            this.state[0]['subCorridor'][i] = [{
-                light: 'ON',
-                AC: 'ON'
-            }]
-        }
+        this.printState('Default State');
     }
 
-    getState(){
+
+    getState() {
         return this.state;
     }
 
-    printState(){
-            this.state.forEach(state => {
-                console.log(state.name);
-                console.log('Main Corridor');
-                console.log(state.mainCorridor);
-                console.log('Sub Corridor');
-                console.log(state.mainCorridor);
-            });
+    movementDetection(floor, subCorridor) {
+        if (!floor || !subCorridor) {
+            return; //floor or Sub Corridor cannot be empty
+        }
+
+        this.currentFloorNumber = subCorridor - 1;
+        this.currentSubCorridorNumber = floor - 1;
+        this.currentFloor = this.state[floor - 1];
+        this.currentSubCorridor = this.state[floor - 1].subCorridor[subCorridor - 1];
+
+        this.state[floor - 1].subCorridor.forEach((value, index) => {
+            if (index === subCorridor - 1) {
+                this.state[floor - 1].subCorridor[index].light = 'ON';
+                this.state[floor - 1].subCorridor[index].AC = 'ON';
+                this.checkAndReducePowerUsage(this.state[floor - 1].subCorridor);
+                return;
+            }
+        });
+
+        this.printState(`Movement in Floor ${floor}, Sub Corridor ${subCorridor} `);
+        this.getCurrentUsage(this.state[floor - 1].subCorridor);
+    }
+
+    movementNotDetection(floor, subCorridor) {
+        if (!floor || !subCorridor) {
+            return; //floor or Sub Corridor cannot be empty
+        }
+
+        this.state[floor - 1].subCorridor.forEach((value, index) => {
+            if (index === subCorridor - 1) {
+                this.state[floor - 1].subCorridor[index].light = 'OFF';
+                this.checkandRevertPowerUsage(this.state[floor - 1].subCorridor);
+                return;
+            }
+
+        });
+
+        this.printState(`No Movement in Floor ${floor}, Sub Corridor ${subCorridor} for a minute`);
+        this.getCurrentUsage(this.state[floor - 1].subCorridor);
+    }
+
+    getMaxAllowedUnit() {
+        return config.floor.subCorridor.maxAllowedPowerConsumption * this.input.subCorridor;
+    }
+
+    checkAndReducePowerUsage(currentState) {
+        let maxPowerUsage = this.getMaxAllowedUnit();
+        this.reducePowerUsage(currentState, maxPowerUsage);
+
+    }
+
+    checkandRevertPowerUsage(currentState) {
+        let maxPowerUsage = this.getMaxAllowedUnit();
+        this.revertPowerUsage(currentState, maxPowerUsage);
+
+    }
+
+    revertPowerUsage(currentState, maxPowerUsage) {
+        this.currentFloor.subCorridor.forEach((value, index) => {
+            let currentUsage = this.getCurrentUsage(currentState);
+            if (currentUsage + config.unit.AC < maxPowerUsage)
+                if (this.currentFloor.subCorridor[this.currentFloorNumber].AC === 'OFF') {
+                    this.currentFloor.subCorridor[this.currentFloorNumber].AC = 'ON';
+                } else {
+                    if (value.AC === 'OFF') {
+                        value.AC = 'ON';
+                    }
+                }
+        })
+    }
+
+
+    reducePowerUsage(currentState, maxPowerUsage) {
+        this.currentFloor.subCorridor.forEach((value, index) => {
+            let currentUsage = this.getCurrentUsage(currentState);
+            if (currentUsage > maxPowerUsage) {
+                if (index === this.currentFloorNumber) {
+                    return;
+                }
+                if (value.AC === 'ON') {
+                    value.AC = 'OFF';
+                }
+            }
+        })
+
+        let currentUsage = this.getCurrentUsage(currentState);
+        if (currentUsage > maxPowerUsage) {
+            if (this.currentFloor.subCorridor[this.currentFloorNumber].AC === 'ON') {
+                this.currentFloor.subCorridor[this.currentFloorNumber].AC = 'OFF';
+            }
+        }
+    }
+
+    getCurrentUsage(currentState) {
+        let currentUsage = 0;
+        currentState.forEach((value, index) => {
+            if (value.AC === 'ON') {
+                currentUsage += config.unit.AC;
+            }
+            if (value.light === 'ON') {
+                currentUsage += config.unit.light;
+            }
+        })
+
+        console.log('Current Usage', currentUsage);
+        return currentUsage;
+
+    }
+
+    turnOffRandomAc() {
+
+    }
+
+    printState(statusMessage) {
+        // this.checkMaxAllowedUnit();
+        console.log(statusMessage);
+        this.state.forEach((floor, index) => {
+            console.log(`Floor ${index + 1}`);
+            floor.mainCorridor.forEach((value, index) => {
+                console.log(`Main Corridor ${index + 1} Light ${index + 1} : ${value.light} AC : ${value.AC}`);
+            })
+            floor.subCorridor.forEach((value, index) => {
+                console.log(`Sub Corridor ${index + 1} Light ${index + 1} : ${value.light} AC : ${value.AC}`);
+            })
+        })
+        console.log("_________________________________");
     }
 }
 
--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+const app = require('express')();
 const floor = require('./controller/floor');
 const config = require('./config/config.json');
-const input = config.input;
+const input = require('./input/input.json');
+var floorInstance;
 
-const floorInstance = new floor(input.mainCorridor, input.subCorridor);
-floorInstance.printState();
-// const state = floorInstance.getState();
-// console.log(JSON.stringify(state, null, "  "));
+function motionDetected() {
+    floorInstance.movementDetection(pfloor = 1, subCorridor = 1);
+    floorInstance.movementDetection(pfloor = 1, subCorridor = 2);
+    floorInstance.movementNotDetection(pfloor = 1, subCorridor = 1);
+    floorInstance.movementNotDetection(pfloor = 1, subCorridor = 2);
+}
+
+function init() {
+    floorInstance = new floor(input);
+}
+
+app.get('/', (req, res) => {
+    init();
+    motionDetected();
+    res.status(200).json(floorInstance.getState());
+})
+
+app.listen(3001);
